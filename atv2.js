43f91fb62c7cdc1f6156f21e2bcd8369214e75b9
Guilherme Felipe <50681430+Guilherme36@users.noreply.github.com>
2021-09-18 15:43:49 -0300

    Add files via upload

---
+++ atv2.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const quantidadeDeAlunos = 14;
+for (let index = 0; index <= quantidadeDeAlunos; index++) {
+    if (index ===0) {
+        console.log('o número é zero');
+    }
+    else if (index % 2 === 0){
+        console.log(`o número ${index} é par`);
+    } else {
+        console.log(`o número ${index} é impar`);
+    }
+}
